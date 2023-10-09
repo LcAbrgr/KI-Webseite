5fda4e4ef944a70f4b1f9716c7aca59c63101961
Lucia <lucia.auburger@study.hs-duesseldorf.de>
2023-10-09 23:38:28 +0200

    Ein und ausblenden der Teilnehmer

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 function showMore(id) {
-  const SM = "SM" + id;
-  const SL = "SL" + id;
-  document.getElementById(id).style.display = "block";
-  document.getElementById(SM).style.display = "none";
-  document.getElementById(SL).style.display = "block";
+    const SM = 'SM' + id
+    const SL = 'SL' + id
+    document.getElementById(id).style.display = 'block'
+    document.getElementById(SM).style.display = 'none'
+    document.getElementById(SL).style.display = 'block'
+}
+function showMoreGrid(id) {
+    const SM = 'SM' + id
+    const SL = 'SL' + id
+    document.getElementById(id).style.display = 'Grid'
+    document.getElementById(SM).style.display = 'none'
+    document.getElementById(SL).style.display = 'block'
 }
 
 function showLess(id) {
-  const SM = "SM" + id;
-  const SL = "SL" + id;
-  document.getElementById(id).style.display = "none";
-  document.getElementById(SM).style.display = "block";
-  document.getElementById(SL).style.display = "none";
+    const SM = 'SM' + id
+    const SL = 'SL' + id
+    document.getElementById(id).style.display = 'none'
+    document.getElementById(SM).style.display = 'block'
+    document.getElementById(SL).style.display = 'none'
 }
